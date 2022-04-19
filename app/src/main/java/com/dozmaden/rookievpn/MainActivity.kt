17d59bbd1455a31ac0001251f57f0e265037a94b
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-04-19 20:16:28 +0300

    Refactoring

--- app/src/main/java/com/dozmaden/rookievpn/MainActivity.kt
+++ app/src/main/java/com/dozmaden/rookievpn/MainActivity.kt
@@ -1,20 +1,11 @@
 package com.dozmaden.rookievpn
 
-import android.Manifest
-import android.app.AppOpsManager
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.afollestad.materialdialogs.MaterialDialog
 import com.dozmaden.rookievpn.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -40,35 +31,5 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-//        checkPermissions()
-    }
-
-    private fun checkPermissions() {
-        if (!isPermissionGranted(Manifest.permission.READ_PHONE_STATE)) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.READ_PHONE_STATE),
-                100
-            )
-        }
-        val appOpsManager = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
-        val mode = appOpsManager.checkOpNoThrow(
-            "android:get_usage_stats",
-            android.os.Process.myUid(),
-            packageName
-        );
-        if (mode != AppOpsManager.MODE_ALLOWED) {
-            MaterialDialog(this)
-                .title(text = "Permission required")
-                .message(text = "Please grant usage access permission")
-                .positiveButton(text = "ok") {
-                    startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
-                }
-                .show()
-        }
     }
 }
-
-fun Context.isPermissionGranted(permission: String): Boolean =
-    ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED
--- app/src/main/java/com/dozmaden/rookievpn/utils/AppUtilities.kt
+++ app/src/main/java/com/dozmaden/rookievpn/utils/AppUtilities.kt
@@ -2,13 +2,18 @@ package com.dozmaden.rookievpn.utils
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.pm.ResolveInfo
 import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
 import bot.box.appusage.utils.UsageUtils
 import com.dozmaden.rookievpn.R
 
-
 object AppUtilities {
+
+    fun Context.isPermissionGranted(permission: String): Boolean =
+        ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED
+
     fun Context.getAppsList(): List<ResolveInfo> {
         val main = Intent(Intent.ACTION_MAIN, null)
         main.addCategory(Intent.CATEGORY_LAUNCHER);
