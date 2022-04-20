34f9202266e189ea72448998bddf9abecbac8d75
Deniz Ozmaden <dozmaden@edu.hse.ru>
2022-04-20 20:55:33 +0300

    Minor changes

--- app/src/main/java/com/dozmaden/rookievpn/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/dozmaden/rookievpn/ui/home/HomeViewModel.kt
@@ -2,10 +2,6 @@ package com.dozmaden.rookievpn.ui.home
 
 import android.annotation.SuppressLint
 import android.app.Application
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.Network
-import android.net.NetworkCapabilities
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
